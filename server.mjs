d50985f70ff44d07a167718650562c0d99fe28c2
SSumu <sadeepalsumudu@gmail.com>
2025-04-09 12:10:08 +0530

    Updated the server.mjs in the back-end.

--- server.mjs
+++ server.mjs
@@ -12,6 +12,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
